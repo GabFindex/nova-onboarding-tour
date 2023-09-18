3ec3bb3840c497c723efd0902f05e55f5c437803
GabFindex <141757103+GabFindex@users.noreply.github.com>
2023-09-18 16:18:46 -0600

    route test 2

--- resources/js/components/Card.vue
+++ resources/js/components/Card.vue
@@ -19,7 +19,7 @@ onMounted(async () => {
     async function fetchConfig() {
         try {
             const response = await Nova.request().get(
-                "/nova-vendor/onboarding/steps".replace("n", "x")
+                "/nova-vendor/onboarding/steps".replace("\\\/", "/")
             );
 
             const flows = await response.data.flows;
